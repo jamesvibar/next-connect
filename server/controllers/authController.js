fdab989b4fe54bcde04916a5baf388335b868814
James Raphael Vibar <vibarjamess@gmail.com>
2019-06-05 09:28:42 +0800

    init

---
+++ server/controllers/authController.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose')
+const User = mongoose.model('User')
+const passport = require('passport')
+
+exports.validateSignup = (req, res, next) => {
+  req.sanitizeBody('name')
+  req.sanitizeBody('email')
+  req.sanitizeBody('password')
+
+  // Name is non-null and is 4 to 10 characters
+  req.checkBody('name', 'Enter your name').notEmpty()
+  req
+    .checkBody('name', 'Name must be between 4 and 10 characters')
+    .isLength({ min: 4, max: 10 })
+
+  // Email is non-null, valid and normalized
+  req
+    .checkBody('email', 'Enter a valid email')
+    .isEmail()
+    .normalizeEmail()
+
+  // Password is non-null and is 4 to 10 characters
+  req.checkBody('password', 'Enter a password').notEmpty()
+  req
+    .checkBody('password', 'Password must be between 4 and 10 characters')
+    .isLength({ min: 4, max: 10 })
+
+  const errors = req.validationErrors()
+
+  if (errors) {
+    const firstError = errors.map(error => error.msg)[0]
+    return res.status(400).send(firstError)
+  }
+
+  next()
+}
+
+exports.signup = async (req, res) => {
+  const { name, email, password } = req.body
+
+  const user = await new User({
+    name,
+    email,
+    password,
+  })
+
+  await User.register(user, password, (err, user) => {
+    if (err) {
+      return res.status(500).send(err.message)
+    }
+    res.json(user)
+  })
+}
+
+exports.signin = (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
+    if (err) {
+      return res.status(500).json(err.message)
+    }
+    if (!user) {
+      return res.status(400).json(info.message)
+    }
+
+    req.logIn(user, err => {
+      if (err) {
+        return res.status(500).json(err.message)
+      }
+
+      res.json(user)
+    })
+  })(req, res, next)
+}
+
+exports.signout = () => {}
+
+exports.checkAuth = () => {}
